80e50fadd5c6e2685500037ea418ca5352e4b502
ck-ross <ck-ross@users.noreply.github.com>
2019-04-11 20:00:44 -0400

    Shows game differential

--- app/src/main/java/com/totalaxe/totalaxescoring/MainActivity.java
+++ app/src/main/java/com/totalaxe/totalaxescoring/MainActivity.java
@@ -1,7 +1,6 @@
 package com.totalaxe.totalaxescoring;
 
 import android.content.Intent;
-import android.content.pm.ActivityInfo;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,17 +8,20 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
 import java.util.LinkedList;
-import java.util.Stack;
 
 public class MainActivity extends AppCompatActivity {
 
-    int scoreTeamA = 0;
-    int scoreTeamB = 0;
+    int scorePlayerOne = 0;
+    int scorePlayerTwo = 0;
     int playerOneNum = -1;
     int playerTwoNum = -1;
+    int playerOneWins = 0;
+    int playerOneLosses = 0;
+    int playerOneTies = 0;
+    int playerTwoWins = 0;
+    int playerTwoLosses = 0;
+    int playerTwoTies = 0;
     int flag = 0;
     int matchResetFlag = 0;
     int round = 0;
@@ -27,6 +29,8 @@ public class MainActivity extends AppCompatActivity {
     LinkedList<Integer> throwsB = new LinkedList<Integer>();
     LinkedList<Integer> gamesA = new LinkedList<Integer>();
     LinkedList<Integer> gamesB = new LinkedList<Integer>();
+    String playerOne = "Player 1";
+    String playerTwo = "Player 2";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,8 +43,7 @@ public class MainActivity extends AppCompatActivity {
 
         //String playerOne = getIntent().getStringExtra("playerOneName");
         //String playerTwo = getIntent().getStringExtra("playerTwoName");
-        String playerOne = "Player 1";
-        String playerTwo = "Player 2";
+
         //round = Integer.parseInt(getIntent().getStringExtra("round"));
         //playerOneNum = Integer.parseInt(getIntent().getStringExtra("playerOneNum"));
         //playerTwoNum = Integer.parseInt(getIntent().getStringExtra("playerTwoNum"));
@@ -54,29 +57,52 @@ public class MainActivity extends AppCompatActivity {
     public void displayForTeamA(int text) {
         TextView t = (TextView) findViewById(R.id.team_a_score);
         t.setText("" + text);
+        displayLead();
         matchResetFlag = 0;
     }
 
     public void displayForTeamB(int text) {
         TextView t = (TextView) findViewById(R.id.team_b_score);
         t.setText("" + text);
+        displayLead();
         matchResetFlag = 0;
     }
 
+    public void displayLead(){
+        TextView ta = (TextView) findViewById(R.id.team_a_game_lead);
+        TextView tb = (TextView) findViewById(R.id.team_b_game_lead);
+        int scoreDiff = scorePlayerOne - scorePlayerTwo;
+        if (scoreDiff > 0){
+            ta.setText(playerOne + " leads by " + scoreDiff);
+            tb.setText(playerTwo + " down by " + scoreDiff);
+        } else if (scoreDiff < 0){
+            ta.setText(playerOne + " down by " + Math.abs(scoreDiff));
+            tb.setText(playerTwo + " leads by " + (Math.abs(scoreDiff)));
+        }
+        else {
+            if (scorePlayerOne == 0){
+            ta.setText("");
+            tb.setText("");
+            }else{
+            ta.setText("Tied");
+            tb.setText("Tied");
+            }
+        }
+    }
 
     public void subA(View view) {
         int temp;
         temp = Integer.parseInt(view.getTag().toString());
-        scoreTeamA -= temp;
-        displayForTeamA(scoreTeamA);
+        scorePlayerOne -= temp;
+        displayForTeamA(scorePlayerOne);
     }
 
     public void addA(View view) {
         int temp;
         temp = Integer.parseInt(view.getTag().toString());
-        scoreTeamA += temp;
+        scorePlayerOne += temp;
         throwsA.add(temp);
-        displayForTeamA(scoreTeamA);
+        displayForTeamA(scorePlayerOne);
         displayThrowsA(view);
     }
 
@@ -93,16 +119,16 @@ public class MainActivity extends AppCompatActivity {
     public void subB(View view) {
         int temp;
         temp = Integer.parseInt(view.getTag().toString());
-        scoreTeamB -= temp;
-        displayForTeamB(scoreTeamB);
+        scorePlayerTwo -= temp;
+        displayForTeamB(scorePlayerTwo);
     }
 
     public void addB(View view) {
         int temp;
         temp = Integer.parseInt(view.getTag().toString());
-        scoreTeamB += temp;
+        scorePlayerTwo += temp;
         throwsB.add(temp);
-        displayForTeamB(scoreTeamB);
+        displayForTeamB(scorePlayerTwo);
         displayThrowsB(view);
     }
 
@@ -135,27 +161,27 @@ public class MainActivity extends AppCompatActivity {
 
     public void undoA(View view){
         if (throwsA.size() > 0) {
-            scoreTeamA -= throwsA.removeLast();
+            scorePlayerOne -= throwsA.removeLast();
             displayThrowsA(view);
-            displayForTeamA(scoreTeamA);
+            displayForTeamA(scorePlayerOne);
         }
     }
 
     public void undoB(View view){
         if (throwsB.size() > 0) {
-            scoreTeamB -= throwsB.removeLast();
+            scorePlayerTwo -= throwsB.removeLast();
             displayThrowsB(view);
-            displayForTeamB(scoreTeamB);
+            displayForTeamB(scorePlayerTwo);
         }
     }
 
     public void resetScores(View view) {
-        scoreTeamA = 0;
-        scoreTeamB = 0;
+        scorePlayerOne = 0;
+        scorePlayerTwo = 0;
         throwsA.clear();
         throwsB.clear();
-        displayForTeamA(scoreTeamA);
-        displayForTeamB(scoreTeamB);
+        displayForTeamA(scorePlayerOne);
+        displayForTeamB(scorePlayerTwo);
         displayThrowsA(view);
         displayThrowsB(view);
     }
@@ -163,10 +189,10 @@ public class MainActivity extends AppCompatActivity {
     public void submitScores(View view) {
         Intent data = new Intent();
         data.putExtra("playerOneNum", playerOneNum);
-        data.putExtra("playerOneScore", scoreTeamA);
+        data.putExtra("playerOneScore", scorePlayerOne);
         data.putExtra("playerTwoNum", playerTwoNum);
-        data.putExtra("playerTwoScore", scoreTeamB);
-        data.putExtra("round", scoreTeamB);
+        data.putExtra("playerTwoScore", scorePlayerTwo);
+        data.putExtra("round", scorePlayerTwo);
         // Activity finished ok, return the data
         setResult(RESULT_OK, data);
         finish();
@@ -175,6 +201,8 @@ public class MainActivity extends AppCompatActivity {
     public void switchSides(View view){
         TextView playerAScore = (TextView) findViewById(R.id.team_a_score);
         TextView playerBScore = (TextView) findViewById(R.id.team_b_score);
+        TextView playerATotal = (TextView) findViewById(R.id.team_a_total);
+        TextView playerBTotal = (TextView) findViewById(R.id.team_b_total);
         EditText playerOneText = (EditText) findViewById(R.id.team_a_name);
         EditText playerTwoText = (EditText) findViewById(R.id.team_b_name);
         TextView totalA = (TextView) findViewById(R.id.team_a_total);
@@ -190,9 +218,9 @@ public class MainActivity extends AppCompatActivity {
         gamesB = (LinkedList) tempList.clone();
 
         int tempScore = 0;
-        tempScore = scoreTeamA;
-        scoreTeamA = scoreTeamB;
-        scoreTeamB = tempScore;
+        tempScore = scorePlayerOne;
+        scorePlayerOne = scorePlayerTwo;
+        scorePlayerTwo = tempScore;
 
         String tempName = "";
         tempName = playerOneText.getText().toString();
@@ -203,15 +231,19 @@ public class MainActivity extends AppCompatActivity {
         if (flag == 0){
             playerAScore.setTextColor(Color.parseColor("#FF0000"));
             playerBScore.setTextColor(Color.parseColor("#0000FF"));
+            playerATotal.setTextColor(Color.parseColor("#FF0000"));
+            playerBTotal.setTextColor(Color.parseColor("#0000FF"));
         }
         else
         {
             playerAScore.setTextColor(Color.parseColor("#0000FF"));
             playerBScore.setTextColor(Color.parseColor("#FF0000"));
+            playerATotal.setTextColor(Color.parseColor("#0000FF"));
+            playerBTotal.setTextColor(Color.parseColor("#FF0000"));
         }
 
-        displayForTeamA(scoreTeamA);
-        displayForTeamB(scoreTeamB);
+        displayForTeamA(scorePlayerOne);
+        displayForTeamB(scorePlayerTwo);
         displayThrowsA(view);
         displayThrowsB(view);
         displayGamesA(view);
@@ -220,8 +252,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void nextGame(View view){
-        gamesA.add(scoreTeamA);
-        gamesB.add(scoreTeamB);
+        gamesA.add(scorePlayerOne);
+        gamesB.add(scorePlayerTwo);
+        if (scorePlayerOne > scorePlayerTwo){
+            playerOneWins += 1;
+            playerTwoLosses += 1;
+        } else if (scorePlayerOne < scorePlayerTwo){
+            playerOneLosses += 1;
+            playerTwoWins += 1;
+        }else{
+            playerOneTies += 1;
+            playerTwoTies += 1;
+        }
         switchSides(view);
         resetScores(view);
     }
@@ -243,11 +285,28 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void nextMatch(View view) {
+        EditText playerOneText = (EditText) findViewById(R.id.team_a_name);
+        EditText playerTwoText = (EditText) findViewById(R.id.team_b_name);
+
+        playerOneWins = 0;
+        playerOneLosses = 0;
+        playerOneTies = 0;
+        playerTwoWins = 0;
+        playerTwoLosses = 0;
+        playerTwoTies = 0;
+
         matchResetFlag += 1;
         if (matchResetFlag > 1) {
             gamesA.clear();
             gamesB.clear();
             resetScores(view);
+            if (flag == 0){
+                //switches names
+                String tempName = "";
+                tempName = playerOneText.getText().toString();
+                playerOneText.setText(playerTwoText.getText().toString());
+                playerTwoText.setText(tempName);
+            }
             flag = 1;
             switchSides(view);
             matchResetFlag = 0;
